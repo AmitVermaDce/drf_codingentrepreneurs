d4b6ab11e588c5754a244022187121b2d0039f90
AMIT VERMA <amitverma.dce@gmail.com>
2023-04-22 17:18:18 +0530

    Rest Framework view and response
    converted simple view function into rest framework view

--- backend/components/views.py
+++ backend/components/views.py
@@ -1,13 +1,16 @@
-from django.http import JsonResponse
 from django.forms.models import model_to_dict
-import json
 from products.models import Products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
-
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    '''
+    DRF API View
+    '''
     model_data = Products.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=["title","content", "price"])
-    return JsonResponse(data=data)
+    return Response(data=data) 
