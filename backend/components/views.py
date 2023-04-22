d9758d92192605e11679b8c0556136bb4071d2bc
AMIT VERMA <amitverma.dce@gmail.com>
2023-04-22 16:41:29 +0530

    Echo Get Data commit

--- backend/components/views.py
+++ backend/components/views.py
@@ -1,6 +1,20 @@
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
+    # It gives us url query parameters
+    print(request.GET)
+    print(request.POST)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass 
+    data['params'] = dict(request.GET)
+    data['headers'] = request.headers  
+    data['content_type'] = request.content_type
+    print(data)
     return JsonResponse({"message": "Hello there, I am coming from api...."})
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,5 +3,8 @@ import requests
 endpoint = "http://127.0.0.1:8000/api/"
 
 
-response = requests.get(endpoint)
+response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world!!"})
+# json data is the body data in byte string format --> b'{"query": "Hello world!!"}'
 print(response.json()['message']) 
+
+ 
