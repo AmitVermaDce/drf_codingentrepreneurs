fb54cb0372932f4613f300ec0f6c1171fd0a2e19
AMIT VERMA <amitverma.dce@gmail.com>
2023-04-24 12:37:44 +0530

    Injest Data with Rest Framework views

--- backend/components/views.py
+++ backend/components/views.py
@@ -2,19 +2,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.serializers import ProductsSerializer
 from products.models import Products
+from django.http import JsonResponse
 
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs): 
     '''
     DRF API View
     '''
-    instance = Products.objects.all().order_by("?").last()
-    data = {}
-    if instance:
-        data = ProductsSerializer(instance).data
-    # data['headers'] = request.headers
-    data['content_type'] = request.content_type
-    # data['params'] = request.GET
-    return Response(data) 
+    serializer = ProductsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        data = serializer.data
+        return JsonResponse(data) 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import pprint
 endpoint = "http://127.0.0.1:8000/api/"
 
 
-response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world!!"})
+response = requests.post(endpoint, json={"title": "Hello World"})
 # json data is the body data in byte string format --> b'{"query": "Hello world!!"}'
 pprint.pprint(response.json()) 
 
