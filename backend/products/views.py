423acea21d215f4db3b323e294a79b5517ea7066
AMIT VERMA <amitverma.dce@gmail.com>
2023-04-25 16:07:14 +0530

    Mixins and Generic API View
    Similar to function based view (performing everything inside one single function), here we are doing the same activity using Class Based views.

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,7 +2,7 @@ from .models import Products
 from .serializers import ProductsSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Products.objects.all()
@@ -44,6 +44,7 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
 
 product_update_view = ProductUpdateAPIView.as_view()
 
+
 class ProductDeleteAPIView(generics.DestroyAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
@@ -52,5 +53,34 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
     def perform_destroy(self, instance):
         super().perform_destroy(instance)
         
-
 product_destroy_view = ProductDeleteAPIView.as_view()
+
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView):
+
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    lookup_field = "pk"
+    
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+    
+
+product_mixin_view = ProductMixinView.as_view()
